d1fd6a12e2976b2a4217747e42c6923c724199f6
david <david.salas@alumnos.upm.es>
2024-03-26 17:07:48 +0100

    v2

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import telebot
 from constants import TOKEN
+import time
+import _thread
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -10,10 +12,20 @@ def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
 
-#Responde a un mensaje
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+#Responde a un mensaje que no es un comando
+@bot.message_handler(content_types=["text"])
+def mensajes_texto(message):
+    if message.text.startswith("/"):
+
+        bot.send_message(message.chat.id, 'Comando *NO* disponible', parse_mode="MarkdownV2")
+    else:
+        bot.send_message(message.chat.id, "Desea usar alguna función?")
+        #Boton para q elija si o o no y q le apareza todos los comandos/funcionalidades que tenga el bot.
+
+
+
+
+
 
 
 #Main
